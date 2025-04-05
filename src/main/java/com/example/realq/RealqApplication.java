c65cd555fe8ed5a434ef5a1cf94391da55c80e12
tella <yung12366@gmail.com>
2025-04-06 00:57:20 +0900

    [chore] Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/example/realq/RealqApplication.java
@@ -0,0 +1,13 @@
+package com.example.realq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realq/RealqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
