f7049a88c3a2c0e137deb3762ef5e88994f2a09a
tella <yung12366@gmail.com>
2025-04-07 20:42:33 +0900

    [feat] Region 외부 API 기능 제작

---
+++ src/main/java/com/example/realq/domain/region/dto/response/RegionItem.java
@@ -0,0 +1,11 @@
+package com.example.realq.domain.region.dto.response;
+
+public record RegionItem(
+        String dataTime,
+        String pm10Value,
+        String pm25Value,
+        String o3Value,
+        String no2Value,
+        String coValue,
+        String so2Value
+) {}
---
+++ src/main/java/com/example/realq/domain/region/repository/RegionRepository.java
@@ -0,0 +1,4 @@
+package com.example.realq.domain.region.repository;
+
+public class RegionRepository {
+}
