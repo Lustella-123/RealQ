53523af4fc63fed6ac56b0ed7390e4cb5d16c785
tella <yung12366@gmail.com>
2025-04-08 10:17:54 +0900

    [refactor] Region, Station 엔드포인트 수정

--- src/main/java/com/example/realq/domain/region/controller/RegionController.java
+++ src/main/java/com/example/realq/domain/region/controller/RegionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/region")
+@RequestMapping("/api/regions")
 @RequiredArgsConstructor
 public class RegionController {
 
