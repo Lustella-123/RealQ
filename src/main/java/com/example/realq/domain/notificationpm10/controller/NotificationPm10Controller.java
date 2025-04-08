aa8f2aeb28d4079322cac9d203f1f8e805de576b
backswan <rnrwprh3434@gmail.com>
2025-04-08 18:21:55 +0900

    [chore] 사용하지 않는 import 삭제

--- src/main/java/com/example/realq/domain/notificationpm10/controller/NotificationPm10Controller.java
+++ src/main/java/com/example/realq/domain/notificationpm10/controller/NotificationPm10Controller.java
@@ -9,7 +9,6 @@ import com.example.realq.domain.notificationpm10.service.NotificationPm10Service
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
